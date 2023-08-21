7531f07b09d0f831cfa8214841179a7b4aaea895
Kris McNair <kmcnair_100@outlook.com>
2023-08-20 20:02:35 -0400

    complete Loader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { logo } from "./assets";
+import { CreatePost, Home } from "./pages";
 
 const App = () => {
   return (
@@ -16,7 +17,12 @@ const App = () => {
           Create
         </Link>
       </header>
-      <main></main>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 };
